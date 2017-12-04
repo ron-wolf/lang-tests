2ade2de2301b05e61c6f48a9bc016c59cac10b9b
Ron Wolf <ronatwork246@gmail.com>
2017-12-04 15:16:17 -0500

    Mess around with ways to make code neater
    …and potentially even less comprehensible. Definitely not intended as an actual suggestion, as it really doesn’t change the functionality in the slightest, introduces some generic functions that are only used once, and hasn’t been tested at all. Nevertheless, it does show off some neat ways to highlight patterns in your Haskell code. Hopefully you get some nuggets out of it.

--- haskell/runsels.hs
+++ haskell/runsels.hs
@@ -1,48 +1,57 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
-module Main where
+{-# LANGUAGE LambdaCase #-}
+module Main (main, getProgName, absPath) where
 
 import Foreign
 import Foreign.C
 
 import System.Environment (getArgs)
 import System.FilePath
-import System.Directory (getCurrentDirectory)
+import System.Directory   (getCurrentDirectory)
 
 import Config
 import Apps
 import Selections (loadSelections)
-import Run (executeSelections)
+import Run     (executeSelections)
 
 main :: IO ()
-main = do progPath <- getFullProgName
-	  absProgPath <- absolute_path progPath
-	  conf <- Config.getDefaultConfig (dropFileName absProgPath)
-	  argv <- getArgs
-	  case argv of
-	       [] -> error "Syntax: runsels [APP | SELECTIONS] [ARGS]"
-	       (appOrSels:args) -> do
-			app <- lookupApp appOrSels conf
-			let selsPath = case app of
-				Nothing -> appOrSels
-				Just path -> path </> "selections.xml"
-			sels <- loadSelections selsPath
-			executeSelections sels args conf
+main = getArgs >>= headTail
+  (error "Syntax: runsels [APP | SELECTIONS] [ARGS]")
+  \(appOrSels:args) -> do
+    config <- getProgName >>= absPath
+	        >>= Config.getDefaultConfig
+	        .:. dropFileName
+    sels <- config >>= lookupApp appOrSels
+	      >>= maybe appOrSels (</> "selections.xml")
+	      >>= loadSelections
+    executeSelections sels args config
+
+infixl 2 (.:.)
+(.:.) = flip (.)
+
+-- bash-3.2> ghci
+-- Prelude>  :type maybe
+-- maybe :: b -> ( a -> b ) -> Maybe a  -> b
+headTail :: b -> ([a] -> b) -> a -> [a] -> b
+headTail bkp fun list = \case [    ] -> bkp
+			                        [x   ] -> bkp
+			                        [x:xs] -> fun x xs
 
 -- From http://hackage.haskell.org/trac/ghc/ticket/3199
 
-getFullProgName :: IO String
-getFullProgName =
-  alloca $ \ p_argc ->
-  alloca $ \ p_argv -> do
-   getFullProgArgv p_argc p_argv
-   peek p_argv >>= peek >>= peekCString
+getProgName :: IO String
+getProgName = alloca $ \argc
+	         -> alloca $ \argv
+           -> getArgv argc argv
+           >> peekCString =<< peek =<< peek argv
 
 foreign import ccall unsafe "getFullProgArgv"
-    getFullProgArgv :: Ptr CInt -> Ptr (Ptr CString) -> IO ()
+  getArgv :: Ptr CInt -> Ptr (Ptr CString) -> IO ()
  
 -- From http://hackage.haskell.org/packages/archive/MissingH/1.2.0.0/doc/html/System-Path-NameManip.html
-absolute_path :: String -> IO String
-absolute_path path@('/':_) = return path
-absolute_path path = do
-   cwd <- getCurrentDirectory
-   return (cwd ++ "/" ++ path)
+
+absPath :: String -> IO String
+absPath path@('/':_) = return path
+absPath path = do
+  cwd <- getCurrentDirectory
+  return $ cwd++"/"++path
